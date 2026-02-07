613acc7647299baf83c216789cdfa282ea7381b6
IsraelHS07 <00200524@uca.edu.sv>
2026-02-07 15:48:28 -0600

    Funcion crear Estudiante en BST

--- Lab_final.cpp
+++ Lab_final.cpp
@@ -10,4 +10,13 @@ struct Estudiante {
     Estudiante* derecha;
 }; 
 
+Estudiante* crearEstudiante(int carnet, const char* nombre, float nota) {
+    Estudiante* nuevo = new Estudiante;
+    nuevo->carnet = carnet;
+    strcpy(nuevo->nombre, nombre);
+    nuevo->nota = nota;
+    nuevo->izquierdo = NULL;
+    nuevo->derecha = NULL;
+    return nuevo;
+}
 
